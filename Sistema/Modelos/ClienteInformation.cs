533b5ae9a8fd1f4bb9cfc0bbda2747dde469a66d
2024.4.14 <2024.4.14@DESKTOP-OMTIDSQ>
2025-04-30 16:25:44 -0300

    adicionando as informações da tabela ClienteInformation...

--- Sistema/Modelos/ClienteInformation.cs
+++ Sistema/Modelos/ClienteInformation.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace Sistema.Modelos
 {
-    internal class ClienteInformation
+    public class ClienteInformation
     {
+        private int IdCliente { get; set; }
+        private DateTime DataCadastro   { get; set; }
+        private string Cliente { get; set; }
+        private string CPF { get; set; }
+        private string RG { get; set; }
+        private string EstadoCivil { get; set; }
+        private DateTime DataDeNascimento { get; set; }
+        private string Endereco { get; set; }
+        private int Numero { get; set; }
+        private string Bairro { get; set; }
+        private string Cidade { get; set; }
+        private string Estado { get; set; }
+        private string CEP { get; set; }
+        public string Telefone { get; set; }
+        private string Telefone2 { get; set; }
+        private string Referencias { get; set; }
+        private string Observacoes { get; set; }
+
+
+
 
     }
 }
